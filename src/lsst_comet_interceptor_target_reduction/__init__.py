e0563549cf0afa2284980f4ad7a414b9d9750ecc
parthenope-ap <parthenope.ap@gmail.com>
2025-03-07 18:09:16 +0100

    minor changes

--- src/lsst_comet_interceptor_target_reduction/__init__.py
+++ src/lsst_comet_interceptor_target_reduction/__init__.py
@@ -1,3 +1,7 @@
-from .example_module import greetings, meaning
+from .comet import Comet
+from .comet_bean_dao import CometBeanDAO
+from .logger import Logger
+from .montecarlo import MonteCarlo, scoring_fn
+from .plotter import plot_isofote
 
-__all__ = ["greetings", "meaning"]
+__all__ = ["Comet", "CometBeanDAO", "Logger", "MonteCarlo", "scoring_fn", "plot_isofote"]
