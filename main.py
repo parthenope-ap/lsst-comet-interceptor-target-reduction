a3f928b5b72e4b74d1c1086aa229c152f6d34978
parthenope-ap <parthenope.ap@gmail.com>
2025-03-07 18:13:46 +0100

    uploading main files

---
+++ main.py
@@ -0,0 +1,242 @@
+import argparse
+import time
+from pprint import pprint
+from typing import Any
+
+import numpy as np
+from lsst_comet_interceptor_target_reduction.comet import Comet
+from lsst_comet_interceptor_target_reduction.comet_bean_dao import CometBeanDAO
+from lsst_comet_interceptor_target_reduction.logger import Logger
+from lsst_comet_interceptor_target_reduction.montecarlo import MonteCarlo, scoring_fn
+from lsst_comet_interceptor_target_reduction.plotter import plot_isofote
+from tqdm import tqdm
+
+from parse_comet import parse_comet
+
+logger = Logger()
+
+
+def main(args: dict[str, Any]) -> None:
+    """
+    Main function to perform grid search for comet parameters.
+
+    Parameters
+    ----------
+    args : dict[str, Any]
+        Dictionary of command line arguments.
+    """
+
+    # Initialize the parameters
+    pprint(args)
+    params, comet_image = parse_comet(args)
+    comet_name = params["comet_name"]
+    num_iter = params["mc_kwargs"]["num_iter"]
+
+    grids = {
+        "param_3": np.linspace(
+            args["start_param_3"],
+            args["stop_param_3"],
+            args["num_samples_param_3"] + 1
+            if args["num_samples_param_3"] > 1
+            else args["num_samples_param_3"],
+        ),
+        "exp_val": np.linspace(
+            args["start_exp_val"],
+            args["stop_exp_val"],
+            args["num_samples_exp_val"] + 1
+            if args["num_samples_exp_val"] > 1
+            else args["num_samples_exp_val"],
+        ),
+        "k": np.linspace(
+            args["start_k"],
+            args["stop_k"],
+            args["num_samples_k"] + 1 if args["num_samples_k"] > 1 else args["num_samples_k"],
+        ),
+    }
+
+    plot_isofote_kwargs = params["plot_isofote_kwargs"]
+    mc_kwargs = params["mc_kwargs"]
+    scoring_fn_kwargs = params["scoring_fn_kwargs"]
+
+    start_time = time.time()
+    dao = CometBeanDAO(comet_name=params["comet_name"])
+    comet_bean = dao.create_comet_bean()
+    comet = Comet(comet_bean)
+
+    best_fit = {
+        "monte_carlo": None,
+        "ra": None,
+        "g": None,
+        "f": None,
+        "fit_error": np.inf,
+        "k": None,
+        "exp_val": None,
+        "mc_param_3": None,
+    }
+
+    logger.info("Starting grid search...")
+    logger.info("GRID PARAMETERS:")
+    logger.info(
+        f"param_3: np.linspace({grids['param_3'][0]}, {grids['param_3'][-1]}, {len(grids['param_3'])})"
+    )
+    logger.info(
+        f"exp_val: np.linspace({grids['exp_val'][0]}, {grids['exp_val'][-1]}, {len(grids['exp_val'])})"
+    )
+    logger.info(f"k: np.linspace({grids['k'][0]}, {grids['k'][-1]}, {len(grids['k'])})")
+    total_iterations = len(grids["param_3"]) * len(grids["exp_val"]) * len(grids["k"])
+    logger.info(f"Total number of iterations: {total_iterations}")
+
+    pbar = tqdm(
+        total=len(grids["param_3"]) * len(grids["exp_val"]) * len(grids["k"]), desc="Grid search progress"
+    )
+
+    for param_3 in grids["param_3"]:
+        for exp_val in grids["exp_val"]:
+            for k in grids["k"]:
+                # Create a Monte Carlo object with the comet
+                monte_carlo = MonteCarlo(comet=comet, param_3=param_3, exp_val=exp_val, k=k, **mc_kwargs)
+
+                # Compute the model
+                f = monte_carlo.fit()
+
+                # compute fit error
+                ra, g, f, result, de, fit_error = scoring_fn(
+                    comet_image=comet_image,
+                    montecarlo_simulation=f,
+                    perihelion_distance=comet.perihelion_distance,
+                    eccentricity=comet.eccentricity,
+                    anomaly=comet.anomaly,
+                    **scoring_fn_kwargs,
+                )
+
+                # save best fit
+                if fit_error < best_fit["fit_error"]:
+                    best_fit["monte_carlo"] = monte_carlo
+                    best_fit["ra"] = ra
+                    best_fit["g"] = g
+                    best_fit["f"] = f
+                    best_fit["result"] = result
+                    best_fit["fit_error"] = fit_error
+
+                    # optimal free parameters
+                    best_fit["exp_val"] = exp_val
+                    best_fit["k"] = k
+                    best_fit["mc_param_3"] = param_3
+
+                pbar.update(1)
+    pbar.close()
+
+    logger.info("Grid search completed.")
+
+    # dump best fit
+    logger.info("Best fit:")
+    logger.info(f"Fit error: {best_fit['fit_error']}")
+    logger.info(f"exp_val: {best_fit['exp_val']}")
+    logger.info(f"k: {best_fit['k']}")
+    logger.info(f"mc_param_3: {best_fit['mc_param_3']}")
+    logger.info(f"ra: {best_fit['ra']}")
+    logger.info(f"Result: {best_fit['result']}")
+
+    # get the MonteCarlo object corresponding to the best fit
+    monte_carlo = best_fit["monte_carlo"]
+    g = np.transpose(best_fit["g"])
+    f = np.transpose(best_fit["f"])
+    ra = best_fit["ra"]
+
+    plot_isofote(
+        x1=monte_carlo.x1,
+        x2=monte_carlo.x2,
+        y1=monte_carlo.y1,
+        y2=monte_carlo.y2,
+        comet=g,
+        montecarlo_simulation=f,
+        max_montecarlo_value=ra,
+        **plot_isofote_kwargs,
+    )
+
+    # Compute the execution time
+    end_time = time.time()
+    execution_time = end_time - start_time
+
+    # output destination file
+    file_path = f"output/{comet_name}/test_time_results.txt"
+
+    # Open the file in append mode and write the results
+    with open(file_path, "a") as file:
+        file.write(f"{num_iter} iteration, time: {round(execution_time / 60):.2f} min\n")
+
+    logger.info("All operations completed successfully.")
+    logger.info(f"Execution time: {execution_time:.2f} second")
+
+
+if __name__ == "__main__":
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-c", "--comet_name", required=True, help="Name of the comet")
+
+    # TODO: comet arguments
+
+    # grid search arguments
+    ap.add_argument(
+        "--start_param_3",
+        required=True,
+        type=float,
+        default=0.0,
+        help="Starting element of the grid search for param_3",
+    )
+    ap.add_argument(
+        "--stop_param_3",
+        required=True,
+        type=float,
+        default=0.05,
+        help="Stopping element of the grid search for param_3",
+    )
+    ap.add_argument(
+        "--num_samples_param_3",
+        required=True,
+        type=int,
+        default=50,
+        help="Number of grid samples for param_3",
+    )
+    ap.add_argument(
+        "--start_exp_val",
+        required=True,
+        type=float,
+        default=-5.0,
+        help="Starting element of the grid search for exp_val",
+    )
+    ap.add_argument(
+        "--stop_exp_val",
+        required=True,
+        type=float,
+        default=0.0,
+        help="Stopping element of the grid search for exp_val",
+    )
+    ap.add_argument(
+        "--num_samples_exp_val",
+        required=True,
+        type=int,
+        default=50,
+        help="Number of grid samples for exp_val",
+    )
+    ap.add_argument(
+        "--start_k", required=True, type=float, default=1.0, help="Starting element of the grid search for k"
+    )
+    ap.add_argument(
+        "--stop_k", required=True, type=float, default=2.0, help="Stopping element of the grid search for k"
+    )
+    ap.add_argument(
+        "--num_samples_k", required=True, type=int, default=10, help="Number of grid samples for k"
+    )
+    args = vars(ap.parse_args())
+    main(args)
+
+
+# python main.py --comet_name C2016Q2 \
+# --start_param_3 0.01 --stop_param_3 0.01 --num_samples_param_3 1 \
+# --start_exp_val -1.0 --stop_exp_val -1.0 --num_samples_exp_val 1 \
+# --start_k 1.0 --stop_k 1.0 --num_samples_k 1
+
+# python main.py --comet_name C2016Q2 \
+# --start_param_3 0.0 --stop_param_3 0.05 --num_samples_param_3 50 \
+# --start_exp_val 0.0 --stop_exp_val 5.0 --num_samples_exp_val 50 \
+# --start_k 1.0 --stop_k 2.0 --num_samples_k 10
---
+++ parse_comet.py
@@ -0,0 +1,80 @@
+import importlib
+from typing import Any
+
+import pandas as pd
+from lsst_comet_interceptor_target_reduction.logger import Logger
+
+logger = Logger()
+
+
+def parse_comet(args: dict[str, Any]) -> tuple[dict[str, Any], pd.DataFrame]:
+    """
+    Initialize parameters for the comet analysis.
+
+    Parameters
+    ----------
+    args : dict[str, Any]
+        Dictionary of arguments passed to the script.
+
+    Returns
+    -------
+    tuple[dict[str, Any], pd.DataFrame]
+        Dictionary of parameters for the comet analysis.
+        DataFrame containing the comet image data.
+    """
+    comet_name = args["comet_name"]
+    constants_module = importlib.import_module(f"input.{comet_name}.{comet_name}")
+
+    params = {
+        "comet_name": comet_name,
+        "mc_kwargs": {
+            "dim_matrix_image": getattr(constants_module, "nc", 100),
+            "depend": getattr(constants_module, "DEPEND_MONTECARLO", False),
+            "num_iter": getattr(constants_module, "NUM_ITER", 10000),
+            "t_size": getattr(constants_module, "T_SIZE", None),
+            "param_00": getattr(constants_module, "MC_PARAM_00", None),
+            "param_01": getattr(constants_module, "MC_PARAM_01", None),
+            "param_02": getattr(constants_module, "MC_PARAM_02", None),
+            "param_03": getattr(constants_module, "MC_PARAM_03", None),
+            # second free parameter: dust velocity v_d (km/s, 1AU)
+            # "param_3" : getattr(constants_module, "MC_PARAM_3", None),
+            # first free parameter: -1/2 sigma ** 2
+            # "exp_val": getattr(constants_module, "EXP_VAL", None),
+            # third free parameter k (heliocentric distance exponent)
+            # "k" : getattr(constants_module, "K", None),
+        },
+        "scoring_fn_kwargs": {
+            "matrix_size": getattr(constants_module, "nc", 100),
+            "portion_comet": getattr(constants_module, "PORTION_COMET", None),
+            "minimum_size": getattr(constants_module, "MININUM_SIZE", None),
+            "binning": getattr(constants_module, "BINNING", None),
+            "sky_background_value": getattr(constants_module, "SKY_BACKGROUND_VALUE", None),
+            "multiplier": getattr(constants_module, "MULTIPLIER", None),
+            "threshold_min": getattr(constants_module, "THRESHOLD_MIN", None),
+        },
+        "plot_isofote_kwargs": {
+            "ra_constant": getattr(constants_module, "RA_CONSTANT", None),
+            "de_constant": getattr(constants_module, "DE_CONSTANT", None),
+            "om": getattr(constants_module, "OM_RECYCLED", None),
+            "am": getattr(constants_module, "AM_RECYCLED", None),
+            "xi": getattr(constants_module, "XI_RECYCLED", None),
+            "levels": getattr(constants_module, "LEVELS", None),
+            "scaling_factor_arrow": getattr(constants_module, "SCALING_FACTOR_ARROW", None),
+            "head_width": getattr(constants_module, "HEAD_WIDTH", None),
+            "head_length": getattr(constants_module, "HEAD_LENGTH", None),
+            "lw": getattr(constants_module, "LW", None),
+            "plot_multiplier": getattr(constants_module, "PLOT_MULTIPLICATOR", None),
+            "num_iter": getattr(constants_module, "NUM_ITER", None),
+            "comet_name": comet_name,
+            "multiplier": getattr(constants_module, "MULTIPLIER", None),
+            "matrix_size": getattr(constants_module, "nc", None),
+        },
+    }
+
+    try:
+        comet_image = pd.read_csv(f"input/{comet_name}/{comet_name}.txt", sep="\\s+")
+    except FileNotFoundError as e:
+        logger.error(f"File {comet_name}/{comet_name}.txt not found.")
+        raise e
+
+    return params, comet_image
